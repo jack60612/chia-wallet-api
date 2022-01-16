6b02ada358e3c1f8a6809c960ead2740b076a726
Jack Nelson <jack@jacknelson.xyz>
2022-01-16 13:03:03 -0500

    Add Licence to work

--- server/api_server.py
+++ server/api_server.py
@@ -1,3 +1,19 @@
+"""   
+    Copyright 2022 Jack Nelson
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 # Other imports
 from chia.util.bech32m import decode_puzzle_hash
 from chia.util.byte_types import hexstr_to_bytes
